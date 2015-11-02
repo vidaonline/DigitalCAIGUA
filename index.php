18b3c916294fee9990499597aae6d4e8d63cfb46
rogerthedeveloper <rogerthedeveloper@gmail.com>
2015-11-02 10:11:41 -0600

    Clean UP

--- index.php
+++ index.php
@@ -18,28 +18,6 @@
 
 					<div>Password</div>
 
-
-					
-function clientImage(key) {
-
-	if(key != null) {
-
-		$("#client-img").fadeOut(200, function() {
-        $("#client-img").attr({'src':'assets/'+key+".png"});
-
-        		$("img").error(function () {
-		  $(this).unbind("error").attr({'src':'assets/0.png'});
-		});
-        		
-    }).fadeIn(200);
-
-
-	}
-	
-	
-
-}
-
 					
 
 			</div>
